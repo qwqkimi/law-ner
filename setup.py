b4290802b6bb636b318d769508aedaa100053a87
terryluke <westbook@outlook.com>
2020-04-13 18:48:23 +0800

    Initial setup.py commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='cl_ner',
+    author='Terry Luke',
+    version='0.0.1',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    description="law-ner package",
+)
